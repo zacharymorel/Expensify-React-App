8d82b851899e051513c362f87249a4d21bf684d4
Zachary Morel <morelz133@gmail.com>
2017-10-21 16:07:01 -0400

    Adding react babel

---
+++ public/scripts/app.js
@@ -0,0 +1,9 @@
+console.log('App.js is running')
+
+// JSK - Javascript XML
+// Javascript syntax  extension
+
+let template = <p>This is JSX from App.js</p>
+let appRoot = document.getElementById('app')
+
+ReactDOM.render(template, appRoot)
