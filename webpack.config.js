c0b003546032b9b90c9ba058e591175b9c6f9458
Zachary Morel <morelz133@gmail.com>
2017-10-30 14:33:37 -0400

    adopting WebPack

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+// where entry point is?
+// where to output file 
+
+const path = require('path')
+
+
+module.exports = {
+  entry: './src/app.js',
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: 'bundle.js'
+  }
+}
