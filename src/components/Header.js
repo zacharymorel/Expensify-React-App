08db4edc1e1d0a6443b554d24098dab0099e1bd7
Zachary Morel <morelz133@gmail.com>
2017-11-01 12:06:09 -0400

    Styling components with Scss

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,9 +2,11 @@ import React from 'react'
 
 //  Child
 const Header = (props) => (
-  <div>
-    <h1>{props.title}</h1>
-    {props.subtitle && <h2>{props.subtitle}</h2>}
+  <div className="header">
+    <div className="container">
+      <h1 className="header__title">{props.title}</h1>
+      {props.subtitle && <h2 className="header__subtitle">{props.subtitle}</h2>}
+    </div>
   </div>
 )
 
