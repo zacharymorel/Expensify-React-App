3640756875be10cd0e5b8144ee61bb1afd312dd9
Zachary Morel <morelz133@gmail.com>
2017-11-02 14:53:26 -0400

    woking with react-Router

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -1,9 +1,12 @@
 import React from 'react'
 
-const EditExpensePage = () => (
-  <div>
-    This is from my Edit Page Component
-  </div>
-)
+const EditExpensePage = (props) => {
+  console.log(props)
+  return (
+    <div>
+      Editing the expense with id of {props.match.params.id}
+    </div>
+  )
+}
 
 export default EditExpensePage
